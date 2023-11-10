830bd6e97c0cc0522e0e0603642cb03de295b025
gsl-ziedbentahar <zied.ben-tahar-ext@groupeseloger.com>
2023-11-10 12:11:28 +0100

    fix(ssot-consumer): ensure batch item processing from the SSOT consumer SQS

--- samples/ssot-pattern/ssot-consumer/src/ssot-connector/lambda-handlers/process-ssot-events.ts
+++ samples/ssot-pattern/ssot-consumer/src/ssot-connector/lambda-handlers/process-ssot-events.ts
@@ -1,13 +1,21 @@
+import { BatchProcessor, EventType, processPartialResponse } from "@aws-lambda-powertools/batch";
 import { enableLambdaPowertoolsLoggingAndMetrics } from "@shared/cross-cutting/lambda-logging-middleware";
 import { logger } from "@shared/cross-cutting/logger";
-import { Context, SQSEvent } from "aws-lambda";
+import { Context, SQSBatchResponse, SQSEvent, SQSRecord } from "aws-lambda";
 
-export const lambdaHandler = async (event: SQSEvent, context: Context): Promise<void> => {
-    
-    for (let index = 0; index < event.Records.length; index++) {
-        logger.info("Processing events...",JSON.stringify(event.Records[index]));
-    }
-    
+const processor = new BatchProcessor(EventType.SQS);
+
+export const recordHandler = async (record: SQSRecord): Promise<void> => {
+    logger.info("Processing events...",JSON.stringify(record));
+}
+
+export const lambdaHandler = async (event: SQSEvent, context: Context): Promise<SQSBatchResponse> => {
+
+    return processPartialResponse(event, async (record: SQSRecord) => {
+        return await recordHandler(record);
+    }, processor, {
+        context,
+    });
 }
 
 export const handler = enableLambdaPowertoolsLoggingAndMetrics(lambdaHandler);
