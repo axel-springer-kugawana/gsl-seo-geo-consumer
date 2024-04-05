9ecda48ab42d4f5c25f5f87e939298205c70ca44
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-05 10:49:44 +0200

    no message

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "awesome-eagle-dev-tfstate"
-    key     = "cm-sample/local/1.0.0/tfstate"
+    bucket  = "crazy-penguin-${var.environment}-tfstate"
+    key     = "${var.environment}/classified-consumer/terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
   }
