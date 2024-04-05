82a68ba2919da2426a1fc8778e5f06bbb1582210
Yann AVONDO <yann.avondo@groupeseloger.com>
2024-04-05 11:06:34 +0200

    fix

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "crazy-penguin-${var.environment}-tfstate"
-    key     = "${var.environment}/classified-consumer/terraform.tfstate"
+    bucket  = "crazy-penguin-dev-tfstate"
+    key     = "dev/classified-consumer/terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
   }
