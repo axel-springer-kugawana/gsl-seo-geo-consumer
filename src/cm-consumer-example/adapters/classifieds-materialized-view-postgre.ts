65fbd876210ba66d17cc305c41eaf0e34e322cf4
Yann AVONDO <yann.avondo@groupeseloger.com>
2024-04-15 18:03:39 +0200

    Wip classified ingestion

--- src/cm-consumer-example/adapters/classifieds-materialized-view-postgre.ts
+++ src/cm-consumer-example/adapters/classifieds-materialized-view-postgre.ts
@@ -2,10 +2,53 @@ import { logger } from "@shared/cross-cutting/logger";
 import { Classified } from "@shared/models/classified/1.0.0/classified";
 import { randomInt } from "crypto";
 import { Client } from 'pg';
+import {DistributionType, Portal } from '../models/classifiedEnums';
+
+
+export const mapPrice = (
+  data: Classified
+): number | null => {
+
+  const portals = data.visibility.requests;
+  const distributionType = data.data.distributionType;
+  const prices = data.data.prices;
+
+  const { rent, buy, buyAuction, compulsoryAuction } = prices || {}
+  const isDE = [Portal.IMMONET, Portal.IWT].some((portal) => portals?.map((p) => p.portal).includes(portal))
+  const priceMapping = {
+    [DistributionType.RENT]: getRentPrice(rent, isDE),
+    [DistributionType.BUY]: buy?.price ?? buy?.pricePerSqUnit,
+    [DistributionType.BUY_AUCTION]: getBuyAuctionPrice(buyAuction, buy),
+    [DistributionType.COMPULSORY_AUCTION]: getCompulsoryAuctionPrice(compulsoryAuction, buyAuction, buy),
+  }
+  return priceMapping[distributionType]?.amount
+}
+
+const getRentPrice = (
+  rent: Classified['data']['prices']['rent'],
+  isDE: boolean,
+): { amount?: number } => {
+  const hierarchyRentDEMapping = ['baseRent', 'totalRent', 'pricePerSqUnit']
+  const hierarchyRentMapping = ['totalRent', 'baseRent', 'pricePerSqUnit']
+  const selectedKey = isDE ? hierarchyRentDEMapping : hierarchyRentMapping
+  return rent?.[`${selectedKey[0]}`] ?? rent?.[`${selectedKey[1]}`] ?? rent?.[`${selectedKey[2]}`]
+}
+const getBuyAuctionPrice = (
+  buyAuction: Classified['data']['prices']['buyAuction'],
+  buy: Classified['data']['prices']['buy'],
+): { amount?: number } => buyAuction?.minPrice ?? buy?.price ?? buy?.pricePerSqUnit
+const getCompulsoryAuctionPrice = (
+  compulsoryAuction: Classified['data']['prices']['compulsoryAuction'],
+  buyAuction: Classified['data']['prices']['buyAuction'],
+  buy: Classified['data']['prices']['buy'],
+): { amount?: number } => compulsoryAuction?.minimumBid ?? getBuyAuctionPrice(buyAuction, buy)
 
 
 const createOrUpdateClassified = async (id: string, data: Classified): Promise<void> => {
   //	docker run -itd -e POSTGRES_USER=user -e POSTGRES_PASSWORD=user -p 5432:5432 -v /data:/var/lib/postgresql/data --name postgresql postgres
+
+  const price = mapPrice(data) ?? undefined;
+
   try {
 
     // const client = new Client();
@@ -14,7 +57,7 @@ const createOrUpdateClassified = async (id: string, data: Classified): Promise<v
       port: 5432,
       user: 'user',
       password: 'user',
-      database: 'ssot',
+      database: 'user',
     });
 
     await client.connect()
@@ -38,7 +81,6 @@ const createOrUpdateClassified = async (id: string, data: Classified): Promise<v
         CertificateOfEligibilityNeeded,
         LocationInBuilding
     ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)`;
-
     const values = [
       randomInt(45514),
       data.metadata.brand,
@@ -49,7 +91,7 @@ const createOrUpdateClassified = async (id: string, data: Classified): Promise<v
       data.data.location.avivGeoId,
       data.data.location.country,
       data.data.location.postalcode,
-      data.data.prices.buy,
+      price,
       data.data.structure.rooms.numberOfRooms,
       data.data.features,
       data.data.features.furnished,
---
+++ src/cm-consumer-example/models/classifiedEnums.ts
@@ -0,0 +1,19 @@
+
+export enum Portal {
+    SL = 'SL',
+    LI = 'LI',
+    LR = 'LR',
+    BD = 'BD',
+    BUCOM = 'BUCOM',
+    IWB = 'IWB',
+    IWT = 'IWT',
+    IMMONET = 'IMMONET',
+  }
+  
+  export enum DistributionType {
+    BUY = 'BUY',
+    RENT = 'RENT',
+    COMPULSORY_AUCTION = 'COMPULSORY_AUCTION',
+    BUY_AUCTION = 'BUY_AUCTION',
+  }
+  
