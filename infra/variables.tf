7f3d51cf0d8f072d30e385f6172063ced1472d70
Yann AVONDO <yann.avondo@groupeseloger.com>
2024-04-16 17:04:15 +0200

    test fix

--- infra/variables.tf
+++ infra/variables.tf
@@ -79,36 +79,45 @@ variable "classified_management_api" {
   type        = string
 }
 
+variable "aws_account_name" {
+  type        = string
+  description = "AWS account name or workspace."
+}
 
-variable "rds_aurora_name" {
+variable "rds_aurora_name"{
   description = "The name of the aurora cluster"
   type        = string
+  default     = "aviv-seeker-whitelabel-seo-ssot-db"
 }
 
 variable "rds_aurora_username" {
   description = "The username of the admin user of the aurora cluster"
   type        = string
+  default     = "main_user"
 }
 
 variable "rds_aurora_database" {
   description = "The database name of the aurora cluster"
   type        = string
+  default     = "ssot"
 }
 
 variable "rds_aurora_port" {
   description = "The port name of the aurora cluster"
   type        = string
+  default     = 5432
 }
 
 variable "rds_engine_mode" {
   description = "The instance type of the aurora cluster"
   type        = string
-  default      = "db.serverless"
+  default     = "db.serverless"
 }
 
 variable "rds_aurora_postgres_version" {
   description = "The postgres version of the aurora cluster"
   type        = string
+  default     = "16.1"
 }
 
 variable "rds_acu_min" {
@@ -121,9 +130,4 @@ variable "rds_acu_max" {
   description = "max acu for serverless configuration"
   type        = string 
   default     = 128
-}
-
-variable "aws_account_name" {
-  type        = string
-  description = "AWS account name or workspace."
 }
