5f07f31b3d8178c2192ad1a8756749de1593e48b
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-17 11:13:18 +0200

    fix

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -1,42 +1,33 @@
 module "aurora_cluster" {
-  source  = "terraform-aws-modules/rds-aurora/aws"
-  version = "9.3.1"
-
-  name   = "${var.rds_aurora_name}${var.suffix}"
-  engine = "aurora-postgresql"
-
-  //https://stackoverflow.com/questions/56626196/aws-rds-engine-mode-currently-unavailable
-  engine_mode    = "provisioned"
-  instance_class = "db.serverless"
-  engine_version = "16.1"
-
-  vpc_id = var.vpc_id
-
-  subnets              = var.subnets
-  db_subnet_group_name = "main"
-
-  vpc_security_group_ids = [aws_security_group.allow_postgres.id]
-  create_security_group  = false
-
-  apply_immediately         = true
-  final_snapshot_identifier = "${var.rds_aurora_name}${var.suffix}-final-snapshot"
-
-  master_username = var.rds_aurora_username
-  master_password = random_password.rds_aurora_password.result
-  database_name   = var.rds_aurora_database
-  port            = var.rds_aurora_port
-
+  # https://github.com/terraform-aws-modules/terraform-aws-rds-aurora/blob/v9.3.1/examples/serverless/main.tf
+  source                          = "terraform-aws-modules/rds-aurora/aws"
+  version                         = "9.3.1"
+  name                            = "${var.rds_aurora_name}${var.suffix}"
+  engine                          = "aurora-postgresql"
+  engine_mode                     = "provisioned"
+  engine_version                  = "16.1"
+  vpc_id                          = var.vpc_id
+  subnets                         = var.subnets
+  db_subnet_group_name            = "main"
+  vpc_security_group_ids          = [aws_security_group.allow_postgres.id]
+  create_security_group           = false
+  apply_immediately               = true
+  final_snapshot_identifier       = "${var.rds_aurora_name}${var.suffix}-final-snapshot"
+  master_username                 = var.rds_aurora_username
+  master_password                 = random_password.rds_aurora_password.result
+  database_name                   = var.rds_aurora_database
+  port                            = 5432
   enabled_cloudwatch_logs_exports = ["postgresql"]
   monitoring_interval             = 60
   storage_encrypted               = true
-  performance_insights_enabled    = false
-
-  scaling_configuration = {
-    auto_pause               = true
-    min_capacity             = var.rds_acu_min
-    max_capacity             = var.rds_acu_max
-    seconds_until_auto_pause = 300
-    timeout_action           = "ForceApplyCapacityChange"
+  skip_final_snapshot             = true
+  serverlessv2_scaling_configuration = {
+    min_capacity = var.rds_acu_min
+    max_capacity = var.rds_acu_max
   }
-  skip_final_snapshot = true
+  instance_class = "db.serverless"
+  instances = {
+    one = {}
+    two = {}
+  }
 }
