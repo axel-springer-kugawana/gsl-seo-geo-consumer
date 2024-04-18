542a108e6be312d4871421dad49005c6d63ee039
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-18 15:28:44 +0200

    fix

--- infra/modules/rds/secrets.tf
+++ infra/modules/rds/secrets.tf
@@ -26,6 +26,19 @@ resource "aws_secretsmanager_secret" "postgres_uri" {
 }
 
 resource "aws_secretsmanager_secret_version" "postgres_uri" {
-  secret_id     = aws_secretsmanager_secret.postgres_uri.id
-  secret_string = "Host = ${module.aurora_cluster.cluster_endpoint}:${var.rds_aurora_port}; Username = ${var.rds_aurora_username}; Password = ${random_password.rds_aurora_password.result}; Database = ${var.rds_aurora_database}; CommandTimeout=2000; Timeout=1024; KeepAlive=2000"
+  secret_id = aws_secretsmanager_secret.postgres_uri.id
+  # secret_string = "Host = ${module.aurora_cluster.cluster_endpoint}:${var.rds_aurora_port}; Username = ${var.rds_aurora_username}; Password = ${random_password.rds_aurora_password.result}; Database = ${var.rds_aurora_database}; CommandTimeout=2000; Timeout=1024; KeepAlive=2000"
+  secret_string = jsonencode({
+
+
+    "Host"           = module.aurora_cluster.cluster_endpoint
+    "Username"       = var.rds_aurora_username
+    "Password"       = random_password.rds_aurora_password.result
+    "Port"           = var.rds_aurora_port
+    "Database"       = var.rds_aurora_database
+    "CommandTimeout" = 2000,
+    "Timeout"        = 1024
+    "KeepAlive"      = 2000
+  })
+
 }
