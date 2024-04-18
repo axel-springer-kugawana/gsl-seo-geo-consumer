8adb980a55ead5aced0cffbb6b46b42f89e43221
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-18 14:19:39 +0200

    fix secret content

--- infra/modules/rds/secrets.tf
+++ infra/modules/rds/secrets.tf
@@ -21,11 +21,11 @@ resource "aws_secretsmanager_secret_version" "postgres_credentials" {
 }
 
 resource "aws_secretsmanager_secret" "postgres_uri" {
-  name                    = "${var.application}-${var.environment}-${var.ssot_name}-postgres_writer-secret"
+  name                    = "${var.application}-${var.environment}-${var.ssot_name}-postgres_credentials_writer-secret"
   recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "postgres_uri" {
-  secret_id     = aws_secretsmanager_secret.postgres_credentials.id
+  secret_id     = aws_secretsmanager_secret.postgres_uri.id
   secret_string = "Host = ${module.aurora_cluster.cluster_endpoint}:${var.rds_aurora_port}; Username = ${var.rds_aurora_username}; Password = ${random_password.rds_aurora_password.result}; Database = ${var.rds_aurora_database}; CommandTimeout=2000; Timeout=1024; KeepAlive=2000"
 }
