9b180af66869a2a29ef8cb0a0fec3beb9cfd1c4c
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-19 11:22:33 +0200

    fix

--- infra/modules/rds/data.tf
+++ infra/modules/rds/data.tf
@@ -1,3 +1,7 @@
 data "aws_security_group" "lambda_consumer_sg" {
-  name = lower("${var.application}-${var.environment}-process-cm-connector-events-vpc-sg")
+
+  filter {
+    name   = "tag:fms-policy-name"
+    values = ["${var.application}-${var.environment}-process-cm-connector-events-vpc-sg"]
+  }
 }
