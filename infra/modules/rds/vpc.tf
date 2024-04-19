dc8640451b35466340a804b7198a0a06d37a827f
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-19 11:18:09 +0200

    fix

--- infra/modules/rds/vpc.tf
+++ infra/modules/rds/vpc.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_postgres" {
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_lambda_consumer_sg_to_rds" {
-  security_group_id = data.aws_security_group.lambda_consumer_sg[0].id
+  security_group_id = data.aws_security_group.lambda_consumer_sg.security_group_id
   from_port         = 5432
   ip_protocol       = "tcp"
   to_port           = 5432
