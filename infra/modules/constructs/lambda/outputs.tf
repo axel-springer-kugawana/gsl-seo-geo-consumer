f5f36ff201bb02c00593cfc55b6b71d4e8fdfb18
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-19 15:30:07 +0200

    test

--- infra/modules/constructs/lambda/outputs.tf
+++ infra/modules/constructs/lambda/outputs.tf
@@ -6,11 +6,6 @@ output "function_name" {
   value = aws_lambda_function.lambda_function.function_name
 }
 
-#testfu
-# output "sg_id" {
-#   value = aws_security_group.lambda_vpc_sg[0].id
-# }
-
 output "sg_id" {
   description = "The ID of the security group"
   value       = try(aws_security_group.lambda_vpc_sg[0].id, aws_security_group.lambda_vpc_sg[0].id, "")
--- infra/modules/rds/output.tf
+++ infra/modules/rds/output.tf
@@ -18,7 +18,6 @@ output "secret_name" {
   value = aws_secretsmanager_secret.postgres_credentials_writer.name
 }
 
-#testfu
 output "sg_id" {
   description = "The ID of the security group"
   value       = aws_security_group.allow_postgres.id
