a0b5fc7a8b650b9465d28e5714043fc82000c543
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-19 14:35:57 +0200

    test

--- infra/modules/rds/output.tf
+++ infra/modules/rds/output.tf
@@ -17,3 +17,9 @@ output "secret_arn" {
 output "secret_name" {
   value = aws_secretsmanager_secret.postgres_credentials_writer.name
 }
+
+#testfu
+output "sg_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.allow_postgres.id
+}
