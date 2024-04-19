39f6df6bcca932cb297d88a5bc7163b405d2b026
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-19 11:31:58 +0200

    fix naùe

--- infra/modules/rds/data.tf
+++ infra/modules/rds/data.tf
@@ -1,5 +1,4 @@
 data "aws_security_group" "lambda_consumer_sg" {
-  tags = {
-    Name = "${var.application}-${var.environment}-process-cm-connector-events-vpc-sg"
-  }
+  #   name = "${var.application}-${var.environment}-process-cm-connector-events-vpc-sg"
+  name = "cm-sample-dev-process-cm-connector-events-vpc-sg"
 }
