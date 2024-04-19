dc199e13fe78435ea5f4d0fae899c25ff44fe659
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-19 15:02:10 +0200

    rename

--- infra/modules/cm-consumer-example/process-cm-events-lambda.tf
+++ infra/modules/cm-consumer-example/process-cm-events-lambda.tf
@@ -101,7 +101,7 @@ module "process_cm_connector_events_lambda" {
 }
 
 #testfu
-resource "aws_vpc_security_group_ingress_rule" "allow_lambda_consumer_sg_to_rds" {
+resource "aws_vpc_security_group_ingress_rule" "allow_lambda_consumer_sg_to_rds2" {
   security_group_id            = var.rds_sg_id                                   #"sg-09f4cd64832e4faac"
   referenced_security_group_id = module.process_cm_connector_events_lambda.sg_id #sg-0b9b49c4a55d47295" #data.aws_security_group.lambda_consumer_sg.id
   from_port                    = 5432
