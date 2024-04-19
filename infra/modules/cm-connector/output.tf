45d38e9f002bb2666f6d4ac220c6d91917135ace
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-04-19 14:07:28 +0200

    fix

--- infra/modules/cm-connector/output.tf
+++ infra/modules/cm-connector/output.tf
@@ -5,8 +5,3 @@ output "queue_arn" {
 output "queue_id" {
   value = module.connector_internal_queue.queue_id
 }
-
-#testfu
-output "sg_id" {
-  value = module.handle_cm_events_lambda.function_name
-}
