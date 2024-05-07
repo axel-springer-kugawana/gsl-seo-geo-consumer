35d9bf16a7e24e0636521757514cb6fe186ad8bd
Yann AVONDO <yann.avondo@groupeseloger.com>
2024-05-07 15:41:35 +0200

    test

--- src/cm-consumer-example/lambda-handlers/process-cm-connector-events.ts
+++ src/cm-consumer-example/lambda-handlers/process-cm-connector-events.ts
@@ -12,31 +12,33 @@ import { Pool } from "pg";
 import * as fs from 'fs';
 const processor = new BatchProcessor(EventType.SQS);
 
-let apisecrets;
-
-(async () => {
-    apisecrets = await getClassifiedApiSecret();
-    // Vous pouvez également effectuer d'autres opérations avec apisecrets ici
-})();
-
-const pool = new Pool({
-    max: 1,
-    min: 0,
-    idleTimeoutMillis: 120000,
-    connectionTimeoutMillis: 10000,
-    host: apisecrets.Host,
-    port: apisecrets.Port,
-    user: apisecrets.Username,
-    password: apisecrets.Password,
-    database: apisecrets.Database
-});
+const createPoolWithApiSecrets = async () => {
+    try {
+      const apisecrets = await getClassifiedApiSecret(); 
+      const pool = new Pool({
+        max: 1,
+        min: 0,
+        idleTimeoutMillis: 120000,
+        connectionTimeoutMillis: 10000,
+        host: apisecrets.Host,
+        port: apisecrets.Port,
+        user: apisecrets.Username,
+        password: apisecrets.Password,
+        database: apisecrets.Database
+      });
+  
+      return pool;
+    } catch (error) {
+      throw new Error('Erreur when create the pool : ' + error.message);
+    }
+  };
 
 
 export const recordHandler = async (record: SQSRecord, context: Context ): Promise<void> => {
     // const body = fs.readFileSync("cm-consumer-example/lambda-handlers/fakes/231116WBR1KI.json", "utf8");
     const e = JSON.parse(record.body);
     const classifiedId = e.data.classifiedId;
-
+    var pool = await createPoolWithApiSecrets();
     if ((e.type === `${SSotEntityName}.deleted.v1`)) {
         await markClassifiedAsDeleted(pool, context, {
             classifiedId, updateDate: e.data.updateDate
