ad571979b13491d1e41c3e535c092fa5b16b7528
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-05-13 10:28:58 +0200

    clean

--- src/cm-consumer-example/adapters/connectPostGre.ts
+++ src/cm-consumer-example/adapters/connectPostGre.ts
@@ -4,11 +4,11 @@ import { getClassifiedApiSecret } from "./classified-api-secrets";
 
 // const apisecrets = await getClassifiedApiSecret();
 const pool = new Pool({
-  max: 1,
-  min: 0,
-  idleTimeoutMillis: 120000,
-  connectionTimeoutMillis: 10000,
-  host: 'aviv-seeker-whitelabel-seo-ssot-db.cluster-ca5oh2kzqupc.eu-west-1.rds.amazonaws.com',//apisecrets.Host,
+  // min: 0,
+  // idleTimeoutMillis: 120000,
+  // connectionTimeoutMillis: 10000,
+
+  host: 'aviv-seeker-whitelabel-seo-ssot-db-two.ca5oh2kzqupc.eu-west-1.rds.amazonaws.com',//apisecrets.Host,
   port: 5432,// #apisecrets.Port,
   user: 'main_user',//apisecrets.Username,
   password: 'DUw?5H!{K(xodHYw',//apisecrets.Password,
