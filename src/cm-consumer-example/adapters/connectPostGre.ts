ccdaf40e3b39caaacca832a3ea87adc4fdc03765
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-05-14 17:21:03 +0200

    put secrets

--- src/cm-consumer-example/adapters/connectPostGre.ts
+++ src/cm-consumer-example/adapters/connectPostGre.ts
@@ -4,30 +4,36 @@ import { Pool } from "pg";
 import { getClassifiedApiSecret } from "./classified-api-secrets";
 
 class CustomPool {
-  private static instance: CustomPool;
+  private static abc: CustomPool;
   private poolObject: Pool | null = null;
   private constructor() {
     //
   }
   static getInstance(): CustomPool {
-    if (!CustomPool.instance) {
-      return (CustomPool.instance = new CustomPool());
+    if (!CustomPool.abc) {
+      return (CustomPool.abc = new CustomPool());
     }
-    return CustomPool.instance;
+    return CustomPool.abc;
   }
 
   getPool = async (): Promise<Pool> => {
     if (!this.poolObject) {
+      const apisecrets = await getClassifiedApiSecret();
+
       this.poolObject = new Pool({
         // min: 0,
         // idleTimeoutMillis: 120000,
         // connectionTimeoutMillis: 10000,
-
-        host: "aviv-seeker-whitelabel-seo-ssot-db-two.ca5oh2kzqupc.eu-west-1.rds.amazonaws.com", //apisecrets.Host,
-        port: 5432, // #apisecrets.Port,
-        user: "main_user", //apisecrets.Username,
-        password: "DUw?5H!{K(xodHYw", //apisecrets.Password,
-        database: "ssot", //apisecrets.Database
+        host: apisecrets.Host,
+        port: apisecrets.Port,
+        user: apisecrets.Username,
+        password: apisecrets.Password,
+        database: apisecrets.Database
+        // host: "aviv-seeker-whitelabel-seo-ssot-db-two.ca5oh2kzqupc.eu-west-1.rds.amazonaws.com", //apisecrets.Host,
+        // port: 5432, // #apisecrets.Port,
+        // user: "main_user", //apisecrets.Username,
+        // password: "DUw?5H!{K(xodHYw", //apisecrets.Password,
+        // database: "ssot", //apisecrets.Database
       });
     }
     return this.poolObject;
