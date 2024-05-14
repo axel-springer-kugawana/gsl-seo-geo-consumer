972244c0c0b0ccfb01b4a42c5ac995e2d9b027da
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-05-14 17:12:14 +0200

    test

--- src/cm-consumer-example/adapters/classifieds-materialized-view-postgre.ts
+++ src/cm-consumer-example/adapters/classifieds-materialized-view-postgre.ts
@@ -1,7 +1,7 @@
 import { logger } from "@shared/cross-cutting/logger";
 import { Classified, Location } from "@shared/models/classified/1.0.0/classified";
 import { mapFeatures, mapPrice, mapGeoAsync } from '../utils/mappingHelpers';
-import { pool } from "./connectPostGre";
+import { poolInstance } from "./connectPostGre";
 import { Context } from "aws-lambda";
 
 const markClassifiedAsDeleted = async (context: Context, deleteCommand: { classifiedId: string, updateDate: string }): Promise<void> => {
@@ -11,11 +11,11 @@ const markClassifiedAsDeleted = async (context: Context, deleteCommand: { classi
   //const client = await pool.connect()
 
   const query = `DELETE FROM Classified WHERE ClassifiedId=$1`;
-  
+  const pool = poolInstance.getPool
   pool().then(async (_pool) => {
     const client = await _pool.connect();
     try {
-       client.query(query, values);
+      client.query(query, values);
     }
     catch (e) {
       if (e.name === "ConditionalCheckFailedException") {
@@ -30,7 +30,7 @@ const markClassifiedAsDeleted = async (context: Context, deleteCommand: { classi
       client.release();
     }
   })
-  
+
 
   /*try {
     await client.query(query, values);
@@ -52,22 +52,22 @@ const markClassifiedAsDeleted = async (context: Context, deleteCommand: { classi
 const createOrUpdateClassified = async (context: Context, id: string, classified: Classified): Promise<void> => {
   context.callbackWaitsForEmptyEventLoop = false; // !important to reuse pool
   try {
-    //const client = await pool.connect()
+    const pool = poolInstance.getPool
     pool().then(async (_pool) => {
-    const client = await _pool.connect();
-    
-    const price = mapPrice(classified) ?? undefined;
-    const features = mapFeatures(classified);
-    let avivGeoIdWkg = ''
-    let doGeoMapping = true;
+      const client = await _pool.connect();
 
-    const { avivGeoId, geometry } = classified.data?.location;
-    const [lon, lat] = geometry?.coordinates ?? [];
+      const price = mapPrice(classified) ?? undefined;
+      const features = mapFeatures(classified);
+      let avivGeoIdWkg = ''
+      let doGeoMapping = true;
 
-    avivGeoIdWkg = avivGeoId;
-    if (avivGeoIdWkg !== undefined || geometry !== undefined) {
+      const { avivGeoId, geometry } = classified.data?.location;
+      const [lon, lat] = geometry?.coordinates ?? [];
 
-      const geoQueryExists = `
+      avivGeoIdWkg = avivGeoId;
+      if (avivGeoIdWkg !== undefined || geometry !== undefined) {
+
+        const geoQueryExists = `
         select avivgeoid
         from(
         select avivgeoid from geo_lat_lon
@@ -76,57 +76,57 @@ const createOrUpdateClassified = async (context: Context, id: string, classified
         select avivgeoid from geo
         where avivgeoid = $3) tmp`;
 
-      const geoValueExists = [
-        lat,
-        lon,
-        avivGeoIdWkg
-      ]
+        const geoValueExists = [
+          lat,
+          lon,
+          avivGeoIdWkg
+        ]
 
-      let existsRecords = (await client.query(geoQueryExists, geoValueExists)).rows[0];
-      if (existsRecords?.avivgeoid !== undefined) {
-        avivGeoIdWkg = existsRecords.avivgeoid
-        doGeoMapping = false;
+        let existsRecords = (await client.query(geoQueryExists, geoValueExists)).rows[0];
+        if (existsRecords?.avivgeoid !== undefined) {
+          avivGeoIdWkg = existsRecords.avivgeoid
+          doGeoMapping = false;
+        }
       }
-    }
-    if (doGeoMapping) {
-      const geo = await mapGeoAsync(classified?.data?.location);
-      let geoLevel = null
+      if (doGeoMapping) {
+        const geo = await mapGeoAsync(classified?.data?.location);
+        let geoLevel = null
 
-      if ((geo == undefined || geo.length == 0)) {
-        geoLevel = 200
-        avivGeoIdWkg = classified?.data?.location?.country ?? "undefined_country"
-      }
-      else {
-        if (avivGeoIdWkg === undefined) {
-          avivGeoIdWkg = geo[geo.length - 1]?.id;
-          geoLevel = geo[geo.length - 1]?.level;
+        if ((geo == undefined || geo.length == 0)) {
+          geoLevel = 200
+          avivGeoIdWkg = classified?.data?.location?.country ?? "undefined_country"
         }
         else {
-          geoLevel = single(geo.filter(x => x.id === avivGeoIdWkg))?.level;
+          if (avivGeoIdWkg === undefined) {
+            avivGeoIdWkg = geo[geo.length - 1]?.id;
+            geoLevel = geo[geo.length - 1]?.level;
+          }
+          else {
+            geoLevel = single(geo.filter(x => x.id === avivGeoIdWkg))?.level;
+          }
         }
-      }
-      let countryId = single(geo?.filter(x => x.level === 200))?.id;
-      let regionId = single(geo?.filter(x => x.level === 400))?.id;
-      let microregionId = single(geo?.filter(x => x.level === 500))?.id;
-      let provinceId = single(geo?.filter(x => x.level === 600))?.id;
-      let municipalityID = single(geo?.filter(x => x.level === 800))?.id;
-      let boroughID = single(geo?.filter(x => x.level === 900))?.id;
-      let neighborhoodId = single(geo?.filter(x => x.level === 1000))?.id;
-      let blocId = single(geo?.filter(x => x.level === 1200))?.id;
+        let countryId = single(geo?.filter(x => x.level === 200))?.id;
+        let regionId = single(geo?.filter(x => x.level === 400))?.id;
+        let microregionId = single(geo?.filter(x => x.level === 500))?.id;
+        let provinceId = single(geo?.filter(x => x.level === 600))?.id;
+        let municipalityID = single(geo?.filter(x => x.level === 800))?.id;
+        let boroughID = single(geo?.filter(x => x.level === 900))?.id;
+        let neighborhoodId = single(geo?.filter(x => x.level === 1000))?.id;
+        let blocId = single(geo?.filter(x => x.level === 1200))?.id;
 
-      const geoValue = [
-        avivGeoIdWkg,
-        geoLevel,
-        countryId,
-        regionId,
-        microregionId,
-        provinceId,
-        municipalityID,
-        boroughID,
-        neighborhoodId,
-        blocId
-      ]
-      const geoQuery = `
+        const geoValue = [
+          avivGeoIdWkg,
+          geoLevel,
+          countryId,
+          regionId,
+          microregionId,
+          provinceId,
+          municipalityID,
+          boroughID,
+          neighborhoodId,
+          blocId
+        ]
+        const geoQuery = `
     INSERT INTO geo (
       avivgeoId,
       geoLevel,
@@ -153,48 +153,48 @@ ON CONFLICT (avivgeoId) DO UPDATE
 
 
 
-      await client.query(geoQuery, geoValue);
+        await client.query(geoQuery, geoValue);
 
-      if (lat !== undefined) {
-        const geo_lat_lonValue = [
-          lat,
-          lon,
-          avivGeoIdWkg
-        ]
+        if (lat !== undefined) {
+          const geo_lat_lonValue = [
+            lat,
+            lon,
+            avivGeoIdWkg
+          ]
 
-        const geo_lat_lon = `
+          const geo_lat_lon = `
       INSERT INTO geo_lat_lon (lat, lon, avivgeoId) VALUES ($1, $2, $3)
   ON CONFLICT (lat,lon) DO UPDATE 
         SET    
         avivgeoId = $3`;
-        await client.query(geo_lat_lon, geo_lat_lonValue);
+          await client.query(geo_lat_lon, geo_lat_lonValue);
+        }
       }
-    }
 
 
-    //Mapping : https://avivgroup.atlassian.net/browse/WLSEO-501
-    const classifiedValue = [
-      id,
-      price,
-      avivGeoIdWkg,
-      classified.data.distributionType,
-      classified.data.estateType,
-      classified.data?.estateSubType !== undefined ? Object.values(classified.data?.estateSubType)?.[0] : null,
-      classified.data?.structure?.rooms?.numberOfRooms,
-      classified.data?.features?.furnished,
-      classified.data?.conditions?.yearOfConstruction,
-      classified.data?.management?.rent?.certificateOfEligibilityNeeded,
-      classified.data?.structure?.building?.locationInBuilding,
-      features,
-      classified.data?.location?.country,
-      classified.metadata.brand,
-      classified?.visibility?.requests.map(e => e.portal),
-      classified?.data?.location?.postalcode,
-      lat ?? 0,
-      lon ?? 0,
-    ];
+      //Mapping : https://avivgroup.atlassian.net/browse/WLSEO-501
+      const classifiedValue = [
+        id,
+        price,
+        avivGeoIdWkg,
+        classified.data.distributionType,
+        classified.data.estateType,
+        classified.data?.estateSubType !== undefined ? Object.values(classified.data?.estateSubType)?.[0] : null,
+        classified.data?.structure?.rooms?.numberOfRooms,
+        classified.data?.features?.furnished,
+        classified.data?.conditions?.yearOfConstruction,
+        classified.data?.management?.rent?.certificateOfEligibilityNeeded,
+        classified.data?.structure?.building?.locationInBuilding,
+        features,
+        classified.data?.location?.country,
+        classified.metadata.brand,
+        classified?.visibility?.requests.map(e => e.portal),
+        classified?.data?.location?.postalcode,
+        lat ?? 0,
+        lon ?? 0,
+      ];
 
-    const classifiedQuery = `
+      const classifiedQuery = `
     INSERT INTO Classified (
       ClassifiedId, 
       Price,
@@ -234,11 +234,11 @@ ON CONFLICT (ClassifiedId) DO UPDATE
           lat = $17,
           lon  = $18          ;`;
 
-    await client.query(classifiedQuery, classifiedValue);
+      await client.query(classifiedQuery, classifiedValue);
 
 
-    await client.release();
-  })
+      await client.release();
+    })
   }
   catch (e) {
     if (e.name === "ConditionalCheckFailedException") {
