f7ebb3c5d12d387035860c67fd6ac0466051d085
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-05-14 16:25:02 +0200

    test

--- src/cm-consumer-example/adapters/connectPostGre.ts
+++ src/cm-consumer-example/adapters/connectPostGre.ts
@@ -5,17 +5,79 @@ import { getClassifiedApiSecret } from "./classified-api-secrets";
 
 // export const pool = async () => {
 
-const pool = new Pool({
-  // min: 0,
-  // idleTimeoutMillis: 120000,
-  // connectionTimeoutMillis: 10000,
+// const pool = new Pool({
+//   // min: 0,
+//   // idleTimeoutMillis: 120000,
+//   // connectionTimeoutMillis: 10000,
 
-  host: 'aviv-seeker-whitelabel-seo-ssot-db-two.ca5oh2kzqupc.eu-west-1.rds.amazonaws.com',//apisecrets.Host,
-  port: 5432,// #apisecrets.Port,
-  user: 'main_user',//apisecrets.Username,
-  password: 'DUw?5H!{K(xodHYw',//apisecrets.Password,
-  database: 'ssot'//apisecrets.Database
-});
+//   host: 'aviv-seeker-whitelabel-seo-ssot-db-two.ca5oh2kzqupc.eu-west-1.rds.amazonaws.com',//apisecrets.Host,
+//   port: 5432,// #apisecrets.Port,
+//   user: 'main_user',//apisecrets.Username,
+//   password: 'DUw?5H!{K(xodHYw',//apisecrets.Password,
+//   database: 'ssot'//apisecrets.Database
+// });
+
+
+
+
+const pool = async (): Promise<Pool> => {
+  //  const apiUrl = config.get("cmApiUrl");
+  const apisecrets = await getClassifiedApiSecret();
+
+  // const headers = {
+  //   "ClientId": apisecrets.ClientId,
+  //   "Authorization": apisecrets.Authorization,
+  //   "User-Agent": 'wl-seo/1.0.0',
+  // }
+
+  // const res = await fetch(`${apiUrl}${link}`, {
+  //   headers
+  // });
+
+  // const classified = await res.json() as Classified;
+
+
+  // return classified;
+
+  return new Pool({
+    // min: 0,
+    // idleTimeoutMillis: 120000,
+    // connectionTimeoutMillis: 10000,
+
+    host: 'aviv-seeker-whitelabel-seo-ssot-db-two.ca5oh2kzqupc.eu-west-1.rds.amazonaws.com',//apisecrets.Host,
+    port: 5432,// #apisecrets.Port,
+    user: 'main_user',//apisecrets.Username,
+    password: 'DUw?5H!{K(xodHYw',//apisecrets.Password,
+    database: 'ssot'//apisecrets.Database
+  });
+
+
+}
+
+
+
+
+// const getClassifiedById = async (link: string): Promise<Classified> => {
+//   const apiUrl = config.get("cmApiUrl");
+//   const apisecrets = await getClassifiedApiSecret();
+
+//   const headers = {
+//       "ClientId": apisecrets.ClientId,
+//       "Authorization": apisecrets.Authorization,
+//       "User-Agent": 'wl-seo/1.0.0',
+//   }
+
+//   const res = await fetch(`${apiUrl}${link}`, {
+//       headers
+//   });
+
+//   const classified = await res.json() as Classified;
+
+
+//   return classified;
+
+// }
+
 
 // const apisecrets = await getClassifiedApiSecret();
 // const pool = new Pool({
