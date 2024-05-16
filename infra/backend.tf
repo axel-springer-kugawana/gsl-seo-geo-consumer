0e82d41702357d479f97b21a92f6c854d2751113
Yann AVONDO <yann.avondo@groupeseloger.com>
2024-05-16 15:09:26 +0200

    test

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,13 @@
 terraform {
   backend "s3" {}
 }
+
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config = {
+    bucket  = "crazy-penguin-${environment}-tfstate"
+    key     = "${environment}/classified-consumer/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+}
