82fcc6a3960900303a232f005573014d24d47222
POLIRIS\francois.phu <francois.phu@groupeseloger.com>
2024-05-21 15:38:17 +0200

    test

--- infra/modules/rds/data.tf
+++ infra/modules/rds/data.tf
@@ -8,3 +8,11 @@ locals {
   vpn_cidr_blocks_cloudflare = [for entry in data.aws_ec2_managed_prefix_list.vpn_access_prefix_list_cloudflare.entries : entry.cidr]
   #   account_id                 = data.aws_caller_identity.current.account_id
 }
+
+
+data "aws_vpc" "foundation_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.aws_account_name}"]
+  }
+}
