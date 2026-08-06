c2627b4f18e18621a549bb32d02ea8ef076690a0
sl-francoisphu <francois.phu@aviv-group.com>
2026-08-06 15:44:39 +0200

    fix

--- src/cm-consumer/adapters/classifieds-fifo-materialized-view-postgre.ts
+++ src/cm-consumer/adapters/classifieds-fifo-materialized-view-postgre.ts
@@ -201,7 +201,6 @@ const createOrUpdateClassified = async (context: Context, id: string, classified
             isRangePrice,
             classifiedBusiness,
             space,
-            ssotupdatedate,
             projectId,
             creationdate,
             isSeLogerPortal,
@@ -232,7 +231,7 @@ const createOrUpdateClassified = async (context: Context, id: string, classified
             externalId)
           VALUES (
             $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23
-            , $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, NOW(), $35, $36, $37, $38
+            , $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34,$35, $36, $37, $38
           , $39
           , $40
           , $41
@@ -292,7 +291,6 @@ const createOrUpdateClassified = async (context: Context, id: string, classified
                 isRangePrice = $32,
                 classifiedBusiness=$33,
                 space=$34,
-                ssotupdatedate= NOW(),
                 projectId=$35,
                 creationdate=$36,
                 isSeLogerPortal=$37,
