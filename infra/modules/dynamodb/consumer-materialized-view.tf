b99a535baf2b30ab2cf2125c27d9f35a1a088f9c
francois phu <francois.phu@seloger.com>
2026-09-02 18:07:21 +0200

    fix ci

--- infra/modules/dynamodb/consumer-materialized-view.tf
+++ infra/modules/dynamodb/consumer-materialized-view.tf
@@ -1,13 +1,19 @@
 resource "aws_dynamodb_table" "consumer_materialized_view_table" {
   name         = "${var.application}"
   billing_mode = "PAY_PER_REQUEST"
-range_key    = var.range_key
+  range_key    = var.range_key
 
   hash_key     = var.partition_key
   attribute {
     name = var.partition_key
     type = "S"
   }
+
+  attribute {
+    name = var.range_key
+    type = "S"
+  }
+  
   ttl {
     attribute_name = "expireat"
     enabled        = true
