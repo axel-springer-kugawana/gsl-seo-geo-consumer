0cf7666e36181fc6be86f8ed0486e74d47b63297
francois phu <francois.phu@seloger.com>
2026-09-10 13:02:02 +0200

    Update main.ts

--- src/geo-bulk-load/main.ts
+++ src/geo-bulk-load/main.ts
@@ -5,10 +5,10 @@ import { processMassiveSqlToDynamoDB as pgGeoFullToDynamoDB, processGeoLineageFa
 export { parquetToPG as processMassiveParquetToPostgres, pgGeoFullToDynamoDB as processMassiveSqlToDynamoDB, pgGeoLineageToDynamoDB as processGeoLineageFallbacksToDynamoDB };
 
 if (require.main === module) {
- parquetToPG()  
+ //parquetToPG()  
  // .then(pgGeoLineageToDynamoDB)
-//  pgGeoLineageToDynamoDB()
- //   .then(pgGeoFullToDynamoDB)
+  pgGeoLineageToDynamoDB()
+    .then(pgGeoFullToDynamoDB)
     .catch((error) => {
       logger.error('[ECS Task] ERREUR CRITIQUE :', error);
       process.exitCode = 1;
